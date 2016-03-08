5440511267156491c197a41c737da141b7b92fd6
Epiphanie <landeguy@gmail.com>
2016-03-08 12:54:29 +0100

    docs: Finish the documentation of SpellCard.h

--- SpellCard.h
+++ SpellCard.h
@@ -1,5 +1,5 @@
 /* \file SpellCard.h
- * \brief Specify the class SpellCard, and contains some templates (\todo: move them)
+ * \brief Specify the class SpellCard, and contains some templates (\todo {move them})
  */
 #ifndef HEAD_SPELLCARD
 #define HEAD_SPELLCARD
@@ -14,12 +14,12 @@
  *
  * During the explosion, the child will be only able to do three of these things:
  *   - spawn new bullets through the function spawn(), and modify said spawned bullets
- *   - \todo Summon new SpellCards
- *   - Modify m_Position (\todo For now, the position of the bullet spawned through spawn is m_Position. Later, an offset parameter will change this relatively)
+ *   - \todo{Summon new SpellCards}
+ *   - Modify m_Position (\todo{For now, the position of the bullet spawned through spawn is m_Position. Later, an offset parameter will change this relatively})
  *
- * \todo As of yet, there is no way for explode to know how much time have passed since its last call. There will be a new parameter to indicate that
+ * \todo{As of yet, there is no way for explode to know how much time have passed since its last call. There will be a new parameter to indicate that}
  *
- * Finally, note that update is final, as we want the child to only work during the explosions. \todo Draw is as for now as there is no way to update the position smoothly, but it will eventually be only override
+ * Finally, note that update is final, as we want the child to only work during the explosions. \todo{Draw is as well for now, as there is no way to update the position smoothly, but it will eventually be only override}
  * For better precision about how to create new patterns, see Pattern.h
  *
  * On internal note, this function simply create a delay queue that contains the delay to wait for the explosion _after the previous one_ (this is important, we stock delays relatively, which means that 3 explosions 0.1 seconds appart will be stocked as (0.1, 0.1, 0.1) not (0.1, 0.2, 0.3)
@@ -49,6 +49,17 @@ private:
 	std::list<float> m_ToExplode; /*!< The list of future explosion to arise*/
 };
 
+/*! \brief Creates a bullet of a certain type
+ *
+ * This function creates a bullet of type U using as parameter the position, how many explosions has already happened and how many will there be.
+ *
+ * \tparam U The type of Bullet to be spawned. 
+ * Restriction:
+ * 	- Must have a constructor U(MathVector, int, int)
+ * 	- Must be a subclass of Bullet
+ *
+ * \return A pointer to the bullet that was created. It can be accessed and modified normally.
+ */
 template<typename U>
 Bullet *SpellCard::spawn(void)
 {
@@ -56,6 +67,17 @@ Bullet *SpellCard::spawn(void)
 	return created;
 }
 
+/*! \brief Spawn several bullets of a certain type.
+ *
+ * Similar to spawn(void), this function creates duplicates number of bullet of type U using as parameter the position, how many explosions has already happened, the number of this bullet in the spawning process, and the total number of explosion
+ *
+ *\tparam U The type of Bullet to be spawned. 
+ * Restriction:
+ * 	- Must have a constructor U(MathVector, int, int, int)
+ * 	- Must be a subclass of Bullet
+ *
+ * \return A list of pointers to all the bullets that were created. Each can be accessed and modified normally.
+ */
 template<typename U>
 std::list<Bullet *> SpellCard::spawn(int duplicates)
 {
