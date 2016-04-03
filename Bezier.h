f354e54cd3ecbb738ed005ac18a6fca6caad4f33
Epiphanie <landeguy@gmail.com>
2016-04-03 03:27:48 +0200

    refactor: Delete useless member of BezierLine
    Delete the member m_Coefficient that is unneeded as the barycenter function does
    not use it and the computeFunction function as the magic of capture takes care
    of that for us

--- Bezier.h
+++ Bezier.h
@@ -13,19 +13,16 @@ public:
     ~BezierLine();
 
     const MathVector &operator[](int i) const;
-    MathVector &operator[](int i); 
+    MathVector &operator[](int i);
     void changeAllControlPoints(const std::array<MathVector, N> &a);
-    void changeControlPoint(int i, const MathVector &m);
 
 private:
     static std::array<float, N> computeBinom(void);
     static std::array<float, N> m_Binom;
     static MathVector barycenter(const std::array<float, N> &ponderation, const std::array<MathVector, N> &points);
 
-    void computeFunction(void);
     std::function<MathVector(float)> m_Fun;
     std::array<MathVector, N> m_ControlPoints;
-    std::array<MathVector, N> m_Coefficient;
 };
 
 #include "Bezier.tpp"
